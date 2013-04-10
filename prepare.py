dcbeaed8fa67f0d33266481667f8d56b3fbd1c01
Onkur Sen <onkursen@gmail.com>
2013-04-10 14:41:55 -0500

    Adding all files
    multinomial outputs

---
+++ prepare.py
@@ -0,0 +1,38 @@
+from openpyxl import load_workbook
+from time import time
+
+# READ DATA FILE INTO MEMORY AS WORKBOOK
+print 'Reading data file.'
+t  = time()
+ws = load_workbook(
+  filename = '/Users/mukul/Desktop/gtd_201210dist/gtd_70to90_1012dist.xlsx', 
+  use_iterators = True
+).worksheets[0]
+print 'Done. Time taken: %f secs.\n' % (time()-t)
+
+inputs = []
+outputs = []
+
+# Get relevant values from database and put into input and output vectors for SVM classification
+print 'Parsing database.'
+t = time()
+
+train = open('svm-train.txt', 'w')
+test = open('svm-test.txt', 'w')
+
+k = 0
+for row in ws.iter_rows():
+  if row[0].internal_value == u'eventid': continue
+
+  iyear, imonth, iday, attack_type, targ_type, country = [row[j].internal_value for j in [1,2,3,28,34,7]]
+
+  out_string = '%f %f %f %f %f %d\n' % (iyear, imonth, iday, attack_type, targ_type,country)
+
+  if k % 2 == 0: train.write(out_string)
+  else: test.write(out_string)
+  k += 1
+
+train.close()
+test.close()
+
+print 'Done. Time taken: %f secs.\n' % (time()-t)
---
+++ run.py
@@ -0,0 +1,101 @@
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import SGDClassifier
+from sklearn import preprocessing
+from time import time
+import numpy as np
+
+t0 = time()
+
+classifier = int(raw_input(
+"""
+Enter number corresponding to classifier you would like to use:
+1. Support Vector Machines
+2. Gaussian Naive Bayes
+3. Multinomial Naive Bayes
+4. Stochastic Gradient Descent with Logistic Regression loss function
+"""))
+
+dataset = int(raw_input(
+"""
+Enter number corresponding to data set you would like to use:
+1. First half and second half
+2. Alternating
+3. Alternating with attack type
+4. Alternating with attack type and target type
+"""))
+
+# Assign appropriate datasets
+input_files = ['half', 'alternating', 'alternating-with-attacktype', 'alternating-all']
+filename = input_files[dataset-1]
+
+# Instantiate arrays for inputs and outputs
+train_inputs = []
+train_outputs = np.array([])
+
+test_inputs = []
+test_expected_outputs = np.array([])
+test_actual_outputs = []
+
+# Read training file
+print 'Reading training file'
+t = time()
+for line in open('datasets/%s-train.txt' % filename):
+  i = line.split(' ')
+  train_outputs = np.append(train_outputs, int(i.pop()))
+  train_inputs.append(map(float, i))
+print 'Done. Time taken: %f secs.\n' % (time()-t)
+
+print 'Create classifier'
+t = time()
+clf = None
+
+# No preprocessing for SVMs
+# Otherwise, scale inputs (preprocessing to make more amenable for machine learning)
+if classifier == 1: # Support vector machines
+  clf = SVC()
+elif classifier == 2: # Gaussian Naive Bayes
+  train_inputs = preprocessing.scale(np.array(train_inputs))
+  clf = GaussianNB()
+elif classifier == 3: # Multinomial Naive Bayes
+  # train_inputs = preprocessing.scale(np.array(train_inputs))
+  clf = MultinomialNB()
+elif classifier == 4: # Stochastic gradient descent with logistic regression
+  train_inputs = preprocessing.scale(np.array(train_inputs))
+  clf = SGDClassifier(loss='log')
+print 'Done. Time taken: %f secs.\n' % (time()-t)
+
+print 'Fit classifier'
+t = time()
+clf.fit(train_inputs, train_outputs)
+print 'Done. Time taken: %f secs.\n' % (time()-t)
+
+# Read test file and scale inputs
+print 'Reading test file'
+t = time()
+for line in open('datasets/%s-test.txt' % filename):
+  i = line.split(' ')
+  test_expected_outputs = np.append(test_expected_outputs, int(i.pop()))
+  test_inputs.append(map(float, i))
+
+# Same here: no preprocessing for SVMs
+# Otherwise, scale inputs (preprocessing to make more amenable for machine learning)
+if classifier != 1:
+  test_inputs = preprocessing.scale(np.array(test_inputs))
+print 'Done. Time taken: %f secs.\n' % (time()-t)
+
+print 'Predict for test file'
+t = time()
+test_actual_outputs = [clf.predict(i)[0] for i in test_inputs]
+print 'Done. Time taken: %f secs.\n' % (time()-t)
+
+print 'Compare outputs'
+t = time()
+right = sum(test_actual_outputs == test_expected_outputs)
+wrong = len(test_actual_outputs) - right
+print 'Done. Time taken: %f secs.\n' % (time()-t)
+
+print 'Number right: %d\nNumber wrong: %d' % (right, wrong)
+print 'Prediction rate: %.2f%%' % (100.0 * right/len(test_actual_outputs))
+print 'Total time taken: %f secs.\n' % (time()-t0)
